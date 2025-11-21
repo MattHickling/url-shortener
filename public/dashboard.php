65e8c7daf2b5a67ac30a09cb55a7b80f14886578
Matt Hickling <m.hickling@hotmail.co.uk>
2025-11-21 17:48:10 +0000

    styling and live url added

--- public/dashboard.php
+++ public/dashboard.php
@@ -21,14 +21,22 @@ $userData = $user->getByEmail($email);
 $userId = $userData['id'] ?? null;
 
 ?>
+<link href="../assets/css/bootstrap.min.css" rel="stylesheet">
 
 <h2>Welcome <?php echo ucfirst(htmlspecialchars($currentUser)); ?>!</h2>
-
-<form method="POST">
-    What is the URL that you would like to shorten?: 
-    <input type="text" name="original_url" required>
-    <input type="submit" value="Shorten">
+<div>
+    <form method="POST" class="center-block">
+    <div>
+        What is the URL that you would like to shorten?: 
+    </div>
+    <input type="text" name="original_url" class="form-control mt-2 mb-4"  required>
+    <input type="submit" class="btn btn-primary" class="form-control"  value="Submit">
 </form>
+</div>
+
+
+
+
 
 <?php
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
@@ -41,9 +49,13 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
             if (!empty($result['short_code'])) {
                 $result = $urlService->create($userId, $originalUrl);
                 if ($result['success']) {
-                    echo "Short URL: <a href='https://my-url/r.php?code=" . htmlspecialchars($result['short_code']) . "' target='_blank'>https://my-url/r.php?code=" . htmlspecialchars($result['short_code']) . "</a>";
+                    echo "<div class='h3'><class='text-success'>New URL:</class> <a href='http://localhost:8888/url-shortener/public/r.php?code=" . htmlspecialchars($result['short_code']) . "' target='_blank'>http://localhost:8888/url-shortener/public/r.php?code=" . htmlspecialchars($result['short_code']) . "</a></div>";
+                    
+                    //LIVE
+                    // $baseUrl = "https://matthewhickling.co.uk/"; 
+                    // $shortUrl = $baseUrl . "r.php?code=" . $result['short_code'];
+                    // echo "<div class='h3'>Short URL: <a href='$shortUrl' target='_blank'>$shortUrl</a></div>";
                 }
-
             }
         } else {
             echo '<p>Invalid URL. Please enter a valid URL.</p>';
@@ -51,5 +63,6 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     }
 }
 ?>
+<script src="../assets/js/bootstrap.bundle.min.js"></script>
 
 
